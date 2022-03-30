df977b705c73503d7ab7e84d890075e9cf5bcf34
Edelberto Rösler <edelberto.rosler@sensedia.com>
2022-03-30 09:50:53 -0300

    Criado a classe Conta com os métodos saca, deposita, e transfere

--- projets/banconovo/src/CriarConta.java
+++ projets/banconovo/src/CriarConta.java
@@ -1,15 +1,23 @@
 public class CriarConta {
     public static void main(String[] args) {
         Conta conta1 = new Conta();
-
         conta1.saldo = 500;
         
         Conta conta2 = conta1;                      // conta2 se referencia ao mesmo objeto da conta1
         conta2.saldo += 150;
 
+        // System.out.println(conta1.saldo);
+        // System.out.println(conta1);
+        // System.out.println(conta2);
+
+        Conta conta3 = new Conta();
+        conta3.deposita(1000);
+        // System.out.println(conta3.saldo);
+        conta3.saca(75);
+        // System.out.println(conta3.saldo);
+        conta3.transfere(200, conta1);
         System.out.println(conta1.saldo);
-        System.out.println(conta1);
-        System.out.println(conta2);
+        System.out.println(conta3.saldo);
         
     }
 }
--- projets/banconovo/src/conta.java
+++ projets/banconovo/src/conta.java
@@ -1,6 +1,33 @@
+import java.util.function.Function;
+
 class Conta {
     double saldo;
     int agencia;
     int numero;
     String titular;
+
+    public void deposita(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta contaDestino){
+        if(this.saldo >= valor){
+            this.saca(valor);
+            contaDestino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
